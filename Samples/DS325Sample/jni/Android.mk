becd7dc13d2899f0370ba789997637ead9503ee7
Hirotaka Niisato <niisato@niisato-macbook-air.local>
2014-05-02 02:21:35 +0900

    DS325Sample application build settings.

--- Samples/DS325Sample/jni/Android.mk
+++ Samples/DS325Sample/jni/Android.mk
@@ -3,8 +3,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE     := DS325Camera
-LOCAL_C_INCLUDES += ./jni/include/
+LOCAL_C_INCLUDES += ./include/
 LOCAL_SRC_FILES  := DS325Camera.cpp
-LOCAL_LDLIBS     := -L./jni/libs/ -llog -ljnigraphics -lDS325 -lOpenNI2
+LOCAL_LDLIBS     := -L./libs/ -llog -ljnigraphics -lDS325 -lOpenNI2
 
 include $(BUILD_SHARED_LIBRARY)
